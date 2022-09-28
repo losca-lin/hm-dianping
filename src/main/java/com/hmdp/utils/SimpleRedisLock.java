a6c7963be82dd6d1449b7e4e6f28fafe0429445b
losca <783840358@qq.com>
2022-09-28 16:02:32 +0800

    Lua 脚本改造分布式锁

--- src/main/java/com/hmdp/utils/SimpleRedisLock.java
+++ src/main/java/com/hmdp/utils/SimpleRedisLock.java
@@ -1,17 +1,28 @@
 package com.hmdp.utils;
 
 import cn.hutool.core.lang.UUID;
+import org.springframework.core.io.ClassPathResource;
 import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.data.redis.core.script.DefaultRedisScript;
 
+import java.util.Collections;
 import java.util.concurrent.TimeUnit;
 
 /**
  * @author Losca
  * @date 2022/9/26 13:21
  */
-public class SimpleRedisLock implements ILock{
+public class SimpleRedisLock implements ILock {
     private String name;
     private StringRedisTemplate stringRedisTemplate;
+
+    private static final DefaultRedisScript<Long> UNLOCK_SCRIPT;
+    static {
+        UNLOCK_SCRIPT = new DefaultRedisScript<>();
+        UNLOCK_SCRIPT.setLocation(new ClassPathResource("unlock.lua"));
+        UNLOCK_SCRIPT.setResultType(Long.class);
+    }
+
 
     public SimpleRedisLock(String name, StringRedisTemplate stringRedisTemplate) {
         this.name = name;
@@ -19,7 +30,7 @@ public class SimpleRedisLock implements ILock{
     }
 
     private static final String KEY_PREFIX = "lock:";
-    private static final String ID_PREFIX = UUID.fastUUID().toString(true)+"-";
+    private static final String ID_PREFIX = UUID.fastUUID().toString(true) + "-";
 
     @Override
     public boolean tryLock(long time) {
@@ -32,6 +43,13 @@ public class SimpleRedisLock implements ILock{
 
     @Override
     public void unLock() {
+        String key = KEY_PREFIX +name;
+        String args = ID_PREFIX + Thread.currentThread().getId();
+        stringRedisTemplate.execute(UNLOCK_SCRIPT, Collections.singletonList(key), args);
+    }
+
+    /* @Override
+    public void unLock() {
         //获取当前线程名
         String threadId = ID_PREFIX + Thread.currentThread().getId();
         //获取redis锁
@@ -41,5 +59,5 @@ public class SimpleRedisLock implements ILock{
             stringRedisTemplate.delete(KEY_PREFIX + name);
 
         }
-    }
+    }*/
 }
