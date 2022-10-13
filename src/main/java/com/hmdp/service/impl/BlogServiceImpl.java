990850c40752edb105981df0f7bd36e7325a8d33
losca <783840358@qq.com>
2022-10-13 16:04:22 +0800

    redis sorted_set重新实现点赞功能，另外实现点赞排行榜

--- src/main/java/com/hmdp/service/impl/BlogServiceImpl.java
+++ src/main/java/com/hmdp/service/impl/BlogServiceImpl.java
@@ -1,9 +1,11 @@
 package com.hmdp.service.impl;
 
-import cn.hutool.core.util.BooleanUtil;
+import cn.hutool.core.bean.BeanUtil;
+import cn.hutool.core.util.StrUtil;
 import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
 import com.hmdp.dto.Result;
+import com.hmdp.dto.UserDTO;
 import com.hmdp.entity.Blog;
 import com.hmdp.entity.User;
 import com.hmdp.mapper.BlogMapper;
@@ -15,7 +17,12 @@ import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.stereotype.Service;
 
 import javax.annotation.Resource;
+import java.util.Collections;
 import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+import static com.hmdp.utils.RedisConstants.BLOG_LIKED_KEY;
 
 /**
  * <p>
@@ -46,12 +53,16 @@ public class BlogServiceImpl extends ServiceImpl<BlogMapper, Blog> implements IB
     }
 
     private void isBlogLiked(Blog blog) {
+        UserDTO user = UserHolder.getUser();
+        if (user == null){
+            return;
+        }
         //获取登录用户
-        Long userId = UserHolder.getUser().getId();
-        String key = "blog:liked:" + blog.getId();
+        Long userId = user.getId();
+        String key = BLOG_LIKED_KEY + blog.getId();
         //判断是否点赞
-        Boolean isMember = stringRedisTemplate.opsForSet().isMember(key, userId.toString());
-        blog.setIsLike(BooleanUtil.isTrue(isMember));
+        Double isMember = stringRedisTemplate.opsForZSet().score(key, userId.toString());
+        blog.setIsLike( isMember != null );
     }
 
     @Override
@@ -78,23 +89,23 @@ public class BlogServiceImpl extends ServiceImpl<BlogMapper, Blog> implements IB
     public Result likeBlog(Long id) {
         //获取登录用户
         Long userId = UserHolder.getUser().getId();
-        String key = "blog:liked:" + id;
+        String key = BLOG_LIKED_KEY + id;
         //判断是否点赞
-        Boolean isMember = stringRedisTemplate.opsForSet().isMember(key, userId.toString());
-        if (BooleanUtil.isFalse(isMember)) {
+        Double isMember = stringRedisTemplate.opsForZSet().score(key, userId.toString());
+        if ( isMember == null) {
             //如果未点赞，可以点赞
             //数据库点赞数+1
             boolean isSuccess = update().setSql("liked = liked+1").eq("id", id).update();
             if (isSuccess) {
                 //保存用户到redis的set集合
-                stringRedisTemplate.opsForSet().add(key, userId.toString());
+                stringRedisTemplate.opsForZSet().add(key, userId.toString(), System.currentTimeMillis());
             }
 
         } else {
             boolean isSuccess = update().setSql("liked = liked-1").eq("id", id).update();
             if (isSuccess) {
                 //保存用户到redis的set集合
-                stringRedisTemplate.opsForSet().remove(key, userId.toString());
+                stringRedisTemplate.opsForZSet().remove(key, userId.toString());
             }
         }
         return Result.ok();
@@ -102,8 +113,19 @@ public class BlogServiceImpl extends ServiceImpl<BlogMapper, Blog> implements IB
 
     @Override
     public Result queryBlogLikes(Long id) {
-
-        return null;
+        String key = BLOG_LIKED_KEY + id;
+        //查询点赞前5的用户
+        Set<String> top5 = stringRedisTemplate.opsForZSet().range(key, 0, 4);
+        if (top5.isEmpty() || top5 == null) {
+            return Result.ok(Collections.emptyList());
+        }
+        List<Long> ids = top5.stream().map(s -> Long.valueOf(s))
+                .collect(Collectors.toList());
+        String idStr = StrUtil.join(",", ids);
+        List<User> users = userService.query().in("id", ids).last("order by field(id," + idStr + ")").list();
+        List<UserDTO> userDTOList = users.stream().map(user -> BeanUtil.copyProperties(user, UserDTO.class))
+                .collect(Collectors.toList());
+        return Result.ok(userDTOList);
     }
 
     private void queryBlogUser(Blog blog) {
--- src/test/java/com/hmdp/test/DebugTest.java
+++ src/test/java/com/hmdp/test/DebugTest.java
@@ -1,13 +1,20 @@
 package com.hmdp.test;
 
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
 /**
  * @author Losca
  * @date 2022/10/7 12:29
  */
 public class DebugTest {
     public static void main(String[] args) {
-        int aaa = print("aaa");
-        System.out.println(1);
+        List<Integer> list = Arrays.asList(1, 2, 3, 4, 5, 7,6, 8);
+        list.stream()
+                .filter(x -> x > 5)
+                .collect(Collectors.toList());
+
     }
 
     private static int print(String msg) {
